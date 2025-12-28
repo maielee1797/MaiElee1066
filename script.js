d8bffdf3f7912841c8b997168d8aaebb3917c91b
maielee1797 <maielee1797@gmail.com>
2025-12-28 18:46:42 +0100

    script.js

--- script.js
+++ script.js
@@ -15,16 +15,31 @@ const articles = {
     ]
 };
 
-// Function to display articles in a section with share button
+// Function to display articles in a section with images and share button
 function loadArticles() {
     Object.keys(articles).forEach(category => {
         const ul = document.getElementById(`${category}-articles`);
         ul.innerHTML = "";
         articles[category].forEach(article => {
             const li = document.createElement("li");
+
+            // Article image
+            const img = document.createElement("img");
+            img.src = "https://via.placeholder.com/100"; // placeholder image
+            img.alt = article.title;
+            img.style.width = "100px";
+            img.style.height = "100px";
+            img.style.objectFit = "cover";
+            img.style.marginRight = "10px";
+            img.style.borderRadius = "6px";
+            img.style.verticalAlign = "middle";
+            li.appendChild(img);
+
+            // Article link
             const a = document.createElement("a");
             a.href = article.url;
             a.textContent = article.title;
+            a.style.verticalAlign = "middle";
             li.appendChild(a);
 
             // Social Share button
@@ -41,4 +56,24 @@ function loadArticles() {
 }
 
 // Load articles on page load
-window.onload = loadArticles;
+window.onload = () => {
+    loadArticles();
+    showSlide(currentSlide);
+    setInterval(nextSlide, 4000);
+};
+
+// ---------------- Slider ----------------
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) slide.classList.add("active");
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
